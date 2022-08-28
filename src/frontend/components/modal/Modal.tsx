ed7ecddd5b5f19d8f4b0cd9e7ff8e8aa83aa8eb6
Greg Thompson <thompson.glowe@gmail.com>
2022-08-28 16:15:53 -0500

    modal component w/ state

---
+++ src/frontend/components/modal/Modal.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+import { File } from '../../types';
+
+interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const Modal: React.FunctionComponent<ModalProps> = ({
+  isOpen,
+  children,
+  ...rest
+}) => {
+  return isOpen ? (
+    <div className="Modal" aria-modal {...rest}>
+      <div className="Modal__body">{children}</div>
+    </div>
+  ) : null;
+};
+
+type ModalState = {
+  type: 'file' | 'directory';
+  parent: File;
+} | null;
+
+export interface ModalManagement {
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setModalState: React.Dispatch<React.SetStateAction<ModalState>>;
+}
+
+export const useModal = (): [
+  boolean,
+  React.Dispatch<React.SetStateAction<boolean>>,
+  () => void,
+  ModalState,
+  React.Dispatch<React.SetStateAction<ModalState>>
+] => {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [modalState, setModalState] = React.useState<ModalState>(null);
+  const toggleModal = () => setIsModalOpen((isModalOpen) => !isModalOpen);
+
+  return [isModalOpen, setIsModalOpen, toggleModal, modalState, setModalState];
+};
